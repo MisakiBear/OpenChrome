8992bab4d6b0e730e986587d1409702802d49e98
Misaki Bear <51944655+MisakiBear@users.noreply.github.com>
2020-09-24 20:23:16 +0900

    Initial commit

---
+++ OpenChrome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using ConfReaderLib;
+
+namespace Open_Chrome
+{
+    // Chrome Icon  Artist: firstfear(firstfear.deviantart.com)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var conf = new ConfReader(new[] { "program path", "instance path" }, "Path.txt");
+                var programpath = conf.GetConf("program path");
+                var instancepath = conf.GetConf("instance path");
+                Process.Start(programpath, $"--user-data-dir=\"{instancepath}\"");
+            }
+            catch { }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OpenChrome
- Create and open a chrome instance.
+ Create and open a chrome instance.  
+ Usually, create a chrome shortcut manually with args --user-data-dir="{instancepath}" will work out well.  
+ But this method won't work if the shortcut pinned to the taskbar or Start.  
+
+# How to use
+ Modify the "Path.txt" file to use.
