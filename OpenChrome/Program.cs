619a74a6958d7adfedc51a650d509b5fc5f22d52
Misaki Bear <51944655+MisakiBear@users.noreply.github.com>
2021-01-02 17:24:29 +0900

    Update to net5.0

--- OpenChrome/Program.cs
+++ OpenChrome/Program.cs
@@ -1,21 +1,13 @@
 ﻿using System.Diagnostics;
 using ConfReaderLib;
 
-namespace Open_Chrome
+// Chrome Icon  Artist: firstfear(firstfear.deviantart.com)
+
+try
 {
-    // Chrome Icon  Artist: firstfear(firstfear.deviantart.com)
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                var conf = new ConfReader(new[] { "program path", "instance path" }, "Path.txt");
-                var programpath = conf.GetConf("program path");
-                var instancepath = conf.GetConf("instance path");
-                Process.Start(programpath, $"--user-data-dir=\"{instancepath}\"");
-            }
-            catch { }
-        }
-    }
+    var conf = new ConfReader(new[] { "program path", "instance path" }, "Path.txt");
+    var programpath = conf.GetConf("program path");
+    var instancepath = conf.GetConf("instance path");
+    Process.Start(programpath, $"--user-data-dir=\"{instancepath}\"");
 }
+catch { }
